39bd64aa798aba5fc22545d415d22f2c58ec7452
sampang-rai <rnk_rai@hotmail.com>
2024-07-01 20:34:39 +0100

    created auth and error handler

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/userRoute.js";
+import authRoute from "./routes/authRoute.js";
 
 dotenv.config();
 
@@ -14,6 +16,21 @@ mongoose
   });
 
 const app = express();
+
+app.use(express.json());
 app.listen(3000, () => {
   console.log("Server running on port 3000");
+});
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoute);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
